2ec5fdbffbbb1007edb0fcc6b171c768b2cc3ab3
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:02:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,31 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
+   
+    def initialize(id=nil, name, grade)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+
+  def update
+     sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
+   end
 
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, grade) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end 
+  end
 end
